d34d0c9ee49208e1163082566915720da1f8e221
unknown <augustboesen@gmail.com>
2025-05-14 15:38:53 +0300

    optimized configuration, added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+
 import './globals.css';
 import './mongoconnect.js';
 
@@ -8,9 +9,6 @@ export const metadata: Metadata = {
   generator: 'Next.js',
   manifest: '/manifest.json',
   keywords: ['nextjs', 'next14', 'pwa', 'next-pwa'],
-  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#fff' }],
-  viewport:
-    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
   icons: [
     { rel: 'apple-touch-icon', url: 'icon-192x192.png' },
     { rel: 'icon', url: 'icon-192x192.png' },
